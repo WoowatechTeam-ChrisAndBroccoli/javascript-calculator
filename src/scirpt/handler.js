efb354e4a8aa40268bffa086ecf49141f60132d9
Tanney-102 <dhxodms89@gmail.com>
2021-02-03 17:41:38 +0900

    feat: Calculator 모듈 작성

---
+++ src/scirpt/handler.js
@@ -0,0 +1,15 @@
+
+
+const $total = document.querySelector("#total");
+
+export default function clickHandler(e) {
+  const clickedClassName = e.target.className;
+  switch (clickedClassName) {
+    case "digit":
+
+    case "modifier":
+
+    case "operator":
+
+  }
+}
